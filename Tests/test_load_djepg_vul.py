f8cf93e132c9e3663b51cc34fd7c682f5204b601
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-02-18 22:13:33 +0800

    Update test_load_djepg_vul.py

--- Tests/test_load_djepg_vul.py
+++ Tests/test_load_djepg_vul.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/files/<filename>")
-def test_load_djpeg(self):
+def test_load_djpeg(filename):
   img = Image.open(filename)
   img.load_djpeg()
   eval(filename)
